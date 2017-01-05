078019db7d1cca446049f80fb0d85f523736e7e2
한정욱 <jungwook@iMac.local>
2017-01-05 13:20:58 +0900

    Initial Commit

---
+++ Healthy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Healthy
+//
+//  Created by 한정욱 on 2017. 1. 5..
+//  Copyright © 2017년 SMARTLY CO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Healthy/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Healthy
+//
+//  Created by 한정욱 on 2017. 1. 5..
+//  Copyright © 2017년 SMARTLY CO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
